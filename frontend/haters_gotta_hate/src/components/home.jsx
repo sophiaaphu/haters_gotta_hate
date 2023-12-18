f098824c137627266c1c6e26e9ad26c4c8f500ce
Sophia Phu <sophiatiffphu@gmail.com>
2023-12-17 18:31:10 -0600

    lineheight attempt 3

--- frontend/haters_gotta_hate/src/components/home.jsx
+++ frontend/haters_gotta_hate/src/components/home.jsx
@@ -23,7 +23,7 @@ function Home() {
   return (
     <div>
       <Box sx = {{ textAlign: "center", marginTop:30 }} >
-        <Typography textAlign="center" fontWeight="bold" fontSize="3rem" sx = {{lineHeight:{xs:1.25, s:1.5}}}>
+        <Typography textAlign="center" fontWeight="bold" fontSize="3rem" sx = {{lineHeight:{xs:2.25, s:2.5}}}>
           The internet has <span style={{ color: '#B43757' }}>lots of negativity</span>.
         </Typography>
         <Typography textAlign="center" fontSize="1.4rem" style={{ color: '#363636', marginBottom: 15}}>
